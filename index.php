cf96eb544c02642a11d6304e96472a9d01c6083f
giampiero <giampierocascarano@gmail.com>
2016-06-22 19:39:16 +0200

    Aggiunta funzione findAndCompare()

--- index.php
+++ index.php
@@ -1,27 +1,35 @@
+<?php 
+	function findAndCompare(){
+		if(isset($_POST["sito2"])){
+			?> 
+			<h1>Funziona</h1>
+			
+
+			<!-- QUI VANNO I RISULTATI -->
+
+			<a href="/">Torna alla home</a>
+
+		<?php die(); }
+
+		
+	}
+?>
 <html>
 	<head>
 		<title>Test Instilla</title>
 	</head>
 <body>
 		<?php 
-		if(isset($_POST["sito2"])){
-			?><h1>Funziona</h1>
-			
-		<?php 
-		}else{
-			
+			findAndCompare();
 		?>
-		<form action="/index.php" method="POST">
+		
+	<form action="/" method="POST">
 			<label for="sito1">Primo sito</label>
 			<input type="text" name="sito1" >
 			<label for="sito2">Primo sito</label>
 			<input type="text" name="sito2" >
 			<button type="submit" name="button">Invia</button>
 		</form>
-	
-	<?php } 
-
-	?>
 	
 </body>
 </html>
