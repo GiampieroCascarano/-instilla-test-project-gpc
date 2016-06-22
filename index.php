e50d47c20cd5da96439db7924dd5b7be0b805b40
giampiero <giampierocascarano@gmail.com>
2016-06-22 18:49:53 +0200

    Aggiunta pagina principale con bottone e 2 campi di testo

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+	<head>
+		<title>Test Instilla</title>
+	</head>
+<body>
+	<form action="/" method="POST">
+		<label for="sito1">Primo sito</label>
+		<input type="text" name="sito1" >
+		<label for="sito2">Primo sito</label>
+		<input type="text" name="sito2" >
+		<button type="submit"></button>
+	</form>
+</body>
+</html>
