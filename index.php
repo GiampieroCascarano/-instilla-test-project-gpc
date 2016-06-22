d380f6bb992c8372d1ca7f9a111c92e192d6e221
giampiero <giampierocascarano@gmail.com>
2016-06-22 19:15:06 +0200

    Testing su funzionamento corretto del form

--- index.php
+++ index.php
@@ -3,12 +3,12 @@
 		<title>Test Instilla</title>
 	</head>
 <body>
-	<form action="/" method="POST">
+	<form action="/prova.php" method="POST">
 		<label for="sito1">Primo sito</label>
 		<input type="text" name="sito1" >
 		<label for="sito2">Primo sito</label>
 		<input type="text" name="sito2" >
-		<button type="submit"></button>
+		<button type="submit">Invia</button>
 	</form>
 </body>
 </html>
---
+++ prova.php
@@ -0,0 +1,8 @@
+<html>
+	<head>
+		<title>Test Instilla</title>
+	</head>
+<body>
+	<h1>Funziona</h1>
+</body>
+</html>
