a2e9fdab23e300854ab966a53d0e13bbecf982a2
giampiero <giampierocascarano@gmail.com>
2016-06-23 10:08:19 +0200

    calcolo percentuale su due parole simili

--- index.php
+++ index.php
@@ -1,32 +1,42 @@
 <?php 
-	function findAndCompare(){
-		if(isset($_POST["sito2"])){
-			?> 
-			<h1>Funziona</h1>
-			
-
-			<!-- QUI VANNO I RISULTATI -->
-
-			<a href="/">Torna alla home</a>
-
-		<?php die(); }
-
+function findAndCompare(){
+	$a="primo";
+	$b="secondo";
+	$l1=strlen($a);
+	$l2=strlen($b);
+	$nparuguali=similar_text($a, $b);
+	if($l1>=$l2){
 		
+		$perc=$nparuguali/$l1*100;
+	
+	}else{
+
+		$perc=$nparuguali/$l2*100;
+	
 	}
+	echo $perc;
 ?>
+		<h1>Funziona</h1>		
+		<!-- QUI VANNO I RISULTATI -->
+
+		<a href="/">Torna alla home</a>
+<?php } ?>
 <html>
 	<head>
 		<title>Test Instilla</title>
 	</head>
 <body>
 		<?php 
+		if(isset($_POST["sito2"])){
 			findAndCompare();
+			die();
+		}
 		?>
 		
 	<form action="/" method="POST">
 			<label for="sito1">Primo sito</label>
 			<input type="text" name="sito1" >
-			<label for="sito2">Primo sito</label>
+			<label for="sito2">Secondo sito</label>
 			<input type="text" name="sito2" >
 			<button type="submit" name="button">Invia</button>
 		</form>
